5c859ce522b3ef3ce19e12822e2963afe704fa96
richard <lichard49@gmail.com>
2016-08-11 15:19:23 -0400

    Convert raw features into ext for HTK; also measure time for recognition process

--- app/src/main/java/com/lichard49/myapplication/MainActivity.java
+++ app/src/main/java/com/lichard49/myapplication/MainActivity.java
@@ -8,8 +8,14 @@ import android.util.Log;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,20 +31,50 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try {
-                int value = stringFromJNI("/storage/emulated/legacy/hmm_data/eating_0.ext");
+                String sampleFramePath = "/storage/emulated/legacy/hmm_data/silent_1";
+                File file = new File(sampleFramePath);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                List<String> vectors = new LinkedList<String>();
+                int vectorSize = -1;
+                while((line = reader.readLine()) != null) {
+                    String[] columns = line.split(",");
+                    if(vectorSize == -1) {
+                        vectorSize = columns.length;
+                    } else if(columns.length != vectorSize) {
+                        System.out.println("Expected " + vectorSize + " at line " + vectors.size() + " but got " + columns.length);
+                        return;
+                    }
+                    vectors.add(line);
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                // start measuring time
+                long start = System.nanoTime();
+
+                createExtFile(vectors, vectorSize, sampleFramePath);
+
+                int value = stringFromJNI(sampleFramePath + ".ext");
                 if(value == 123456789) {
-                    File file = new File("/storage/emulated/legacy/hmm_data/recognition_result");
-                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    File file2 = new File("/storage/emulated/legacy/hmm_data/recognition_result");
+                    BufferedReader reader2 = new BufferedReader(new FileReader(file2));
                     StringBuilder result = new StringBuilder();
-                    String line;
-                    while((line = reader.readLine()) != null) {
-                        result.append(line);
+                    String line2;
+                    while((line2 = reader2.readLine()) != null) {
+                        result.append(line2);
                         result.append('\n');
                     }
                     setText(result.toString());
                 } else {
                     setText("Recognition failed");
                 }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                // stop measuring time
+                long end = System.nanoTime();
+
+                Log.d("lichard49", "Time taken " + (end-start));
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -73,5 +109,44 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+    }
+
+    private void createExtFile(List<String> fVectors, int numColumns, String fileName) throws IOException {
+
+        int n_samples = fVectors.size();
+        int sampPeriod = 2000; // default is 2000ns
+        int sampSize = 4;
+        int sizeOfVec = numColumns;
+        int parmKind = 9; // user defined
+
+		/*creates a file and DataOutputStream*/
+        FileOutputStream fout = new FileOutputStream(fileName + ".ext");
+        DataOutputStream dos = new DataOutputStream(fout);
+
+		/*writes 4-byte header*/
+        dos.writeInt(n_samples);	// number of samples
+        dos.writeInt(sampPeriod);	// period
+
+		/*writes 2-byte header*/
+        dos.writeChar(sampSize * sizeOfVec); // size = size of data x size of vector
+        dos.writeChar(parmKind); // type of sample
+
+		/*writes 8-byte feature fectors*/
+        for(int i=0; i<n_samples; i++){
+            String vecs = fVectors.get(i);
+            StringTokenizer st = new StringTokenizer(vecs);
+
+            while(st.hasMoreTokens()){
+                String vec = st.nextToken();
+
+                float f = (new Float(vec)).floatValue();
+
+				/*writes 4-byte vector value*/
+                dos.writeFloat(f);
+            }
+        }
+
+        //close file
+        fout.close();
     }
 }
